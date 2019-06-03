9f895d41395626d134b6d30b5238a11bdba819f0
ejngnng <ztnjhappy@163.com>
2019-06-03 12:02:22 +0800

    sepreat nav and sync led

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,13 @@
 #include <Wire.h>
 #include <FastLED.h>
 
+#define DEBUG_ANYLED_I2C  0
+
 #define ANYLED_I2C_ADDR 0x90
 #define NUM_LEDS    3
 
 CRGB nav_leds[NUM_LEDS];
+CRGB sync_leds[NUM_LEDS];
 
 typedef struct rgb_s{
   uint8_t r;
@@ -17,9 +20,12 @@ rgb_t rgb;
 
 bool update = false;
 
+uint32_t nav_interval = 1000;
+uint32_t last_time = 0;
+
 void receiveEvent(int x);
-void requestEvent();
 void led_loop();
+void nav_loop();
 
 void setup() {
   // put your setup code here, to run once:
@@ -27,51 +33,65 @@ void setup() {
   Wire.setClock(100000);
   Wire.begin(ANYLED_I2C_ADDR);
   Wire.onReceive(receiveEvent);
-  Wire.onRequest(requestEvent);
   memset(&rgb, 0, sizeof(rgb_t));
   FastLED.addLeds<NEOPIXEL, 9>(nav_leds, NUM_LEDS);
   FastLED.addLeds<NEOPIXEL, 8>(nav_leds, NUM_LEDS);
-  FastLED.addLeds<NEOPIXEL, 7>(nav_leds, NUM_LEDS);
-  FastLED.addLeds<NEOPIXEL, 6>(nav_leds, NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL, 7>(sync_leds, NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL, 6>(sync_leds, NUM_LEDS);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   led_loop();
+  nav_loop();
 }
 
 void receiveEvent(int x){
   update = true;
-  while( Wire.available() > 0) // 循环执行，直到数据包只剩下最后一个字符
+  while( Wire.available() > 0)
   {
       rgb.r = Wire.read();
       rgb.g = Wire.read();
       rgb.b = Wire.read();
   }
 
+#if DEBUG_ANYLED_I2C
   Serial.print("r: ");
   Serial.print(rgb.r, HEX);
   Serial.print(" g: ");
   Serial.print(rgb.g, HEX);
   Serial.print(" b: ");
   Serial.println(rgb.b, HEX);
+#endif
 }
 
-//当主机通知从机上传数据，执行该事件
-void requestEvent()
-{
-  Wire.write("hello");
-}
 
 void led_loop(){
   if(update){
     update = false;
     for(uint8_t i=0; i<NUM_LEDS; i++){
-      nav_leds[i] = CRGB(rgb.r, rgb.g, rgb.b);
+      sync_leds[i] = CRGB(rgb.r, rgb.g, rgb.b);
       FastLED.show();
     }
   }else{
     FastLED.show();
   }
 
+}
+
+void nav_loop(){
+    uint32_t now = millis();
+    if(now -last_time <= nav_interval){
+      for(uint8_t i=0; i<NUM_LEDS; i++){
+        nav_leds[i] = CRGB::Red;
+        FastLED.show();
+      }
+    }else if(now - last_time > nav_interval && now - last_time <= 2*nav_interval){
+      for(uint8_t i=0; i<NUM_LEDS; i++){
+        nav_leds[i] = CRGB::Black;
+        FastLED.show();
+      }
+    }else{
+      last_time = now;
+    }
 }
